0a662c44d94cb5822da12fb64803846f3f623073
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2022-03-20 23:55:51 +0900

    Update assert.go

--- testutil/assert.go
+++ testutil/assert.go
@@ -7,6 +7,6 @@ import (
 
 func AssertEqual[V any](t *testing.T, want, got V) {
 	if !reflect.DeepEqual(want, got) {
-		t.Errorf("want='%+v' got='%v+'", want, got)
+		t.Errorf("want='%+v' got='%+v'", want, got)
 	}
 }
